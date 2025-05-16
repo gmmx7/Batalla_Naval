117d99425aa67568c367b23894473ab38d6aeff2
Gamaliel Vargas <gamaliellozanovargas@gmail.com>
2025-05-16 00:50:53 -0600

    Se ha completado la fase 1

--- Funciones.c
+++ Funciones.c
@@ -2,12 +2,24 @@
 #include <stdbool.h>
 #include "Funciones.h"
 
+//Mostrar textos -> Funcionamiento
 void mostrarOpcMenu(){
-	printf("\n-> Menu: \n1. Nuevo Juego \n2. Instrucciones \n3.Salir");
+	printf("\n-> Menu: \n1. Nuevo Juego \n2. Instrucciones \n3. Salir");
 }
-int conversor(char caracter){
+void instrucciones(){
+	printf("\nInstrucciones: \nDo you got plans tonight? \nI'm a couple hundred miles from Japan and I\nI was thinking I could fly to your hotel tonight\n'Cause I-I-I can't get you off my mind\nCan't get you off my mind\nCan't get you off my mind (uh)\n");
+}
+void instruccionesDimensionador(){
+	printf("\n-> Bienvenido al dimensionador!\n\tPara crear el juego necesitamos un numero de filas y\n\tcolumnas mayores que 5 y menores a 15\n********Ingresa tus datos!");
+}
+
+//Funciones globales
+int conversor(char caracter){//Conversor de un solo dígito
 	int entero = caracter - '0';
 	return entero;
+}
+int unirNumeros(int numero, int resultado){
+	return resultado * 10 + numero;
 }
 int convertirEntrada(char entrada[]){
 	int i = 0, resultado = 0, numero = 0;
@@ -25,17 +37,23 @@ int convertirEntrada(char entrada[]){
 		if(entrada[i] >= '0' && entrada[i] <= '9'){
 			caracter = entrada[i];
 			numero = conversor(caracter);
-			resultado = resultado * 10 + numero;
+			resultado = unirNumeros(numero, resultado);
 		}else if(entrada[i] == ' '){
 			printf("NOOOO, un espacio :c\n");
+			return -1;
 		}else{  
-			return resultado;
+			printf("Caracter inválido :c : %c\n", entrada[i]);
+			return -1;
 		}
-
 		i++;
 	}
 	return resultado;
 }
+void limpiarConsola(){
+    printf("\033[H\033[J");
+}
+
+//Fase 1: Menu
 int menu(){
 	char entrada[100];
 	int opcion;
@@ -47,8 +65,11 @@ int menu(){
 		opcion = convertirEntrada(entrada);
 			switch(opcion){
 				case 1:{
-					printf("\nCargando juego...");
 					autorizado = true;
+					limpiarConsola();
+					printf("\nPreparando juego...");
+					//Pedir dimensiones y validar
+					dimensionador();
 					return 0;
 				}
 				case 2:{
@@ -61,13 +82,76 @@ int menu(){
 					exit(0);
 				}
 				default:{
-					printf("\nOpcion incorrecta papu, ingresa una opcion valida");
+					limpiarConsola();
+					printf("Opcion incorrecta papu, ingresa una opcion valida");
+					mostrarOpcMenu();
 					break;
 				}
 			}
 	}while(!autorizado);
 	return -1;
 }
-void instrucciones(){
-	printf("\nInstrucciones: \nDo you got plans tonight? \nI'm a couple hundred miles from Japan and I\nI was thinking I could fly to your hotel tonight\n'Cause I-I-I can't get you off my mind\nCan't get you off my mind\nCan't get you off my mind (uh)\n");
+
+//Fase 2: Creación de tableros
+int validarDimensiones(int filas, int columnas){
+	if((filas >= 5 && filas <= 10) && (columnas >= 5 && columnas <= 15)){
+		return 0; //Dimension dentro del rangooooo
+	}else if((filas >= 5 && filas <= 10) != (columnas >= 5 && columnas <= 15)){
+		if(filas < 5 || filas > 10){
+			return 1; //Error en el numero de filas, es menor a 5 o mayor a 15
+		}else if(columnas < 5 || columnas > 15){
+			return 2; //Error en el numero de columnas, es menor a 5 o mayor a 15
+		}
+	}
+	return -1;
 }
+bool verificarDimensiones(int filas, int columnas) {
+	int posibleError = validarDimensiones(filas, columnas);
+
+	switch(posibleError){
+		case 0:
+			printf("\nDimensiones aceptadas.");
+			return true;
+		case 1:
+			printf("\nDimensiones no aceptadas :c\nTu error esta en las FILAS\nIngresa de nuevo las dimensiones :D\n");
+			break;
+		case 2:
+			printf("\nDimensiones no aceptadas :c\nTu error esta en las COLUMNAS\nIngresa de nuevo las dimensiones :D\n");
+			break;
+		default:
+			printf("\nDimensiones super rechazadas :c\nLas filas y columnas son menores o mayores del rango\nIngresa de nuevo las dimensiones :D\n");
+			break;
+	}
+	return false;
+}
+void dimensionador(){
+	char entrada1[10];
+	char entrada2[10];
+	int filas = 0, columnas = 0, posibleError;
+	bool dimensionadorActivado = false;
+	
+	instruccionesDimensionador();
+	
+	do{
+		printf("\nIngresa las dimensiones: ");
+		printf("\nFilas: ");
+		fgets(entrada1, sizeof(entrada1), stdin);
+		filas = convertirEntrada(entrada1);
+
+		printf("\nColumnas: ");
+		fgets(entrada2, sizeof(entrada2), stdin);
+		columnas = convertirEntrada(entrada2);
+
+		dimensionadorActivado = verificarDimensiones(filas, columnas);
+	}while(!dimensionadorActivado);
+}
+/*
+void mostrarMatriz(char matriz[][], int filas, int columnas){
+	for(int i=0;i<filas;i++){
+		printf(" %s ", matriz[i][]);
+		for(int j=0;i<columnas;j++){
+			printf(" %s", );
+		}
+	}
+}
+*/
--- Funciones.h
+++ Funciones.h
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//Mostrar textos -> Funcionamiento
 void mostrarOpcMenu();
-int conversor(char caracter);
-int convertirEntrada(char entrada[]);
-int menu();
 void instrucciones();
+void instruccionesDimensionador();
+
+//Funciones globales
+int conversor(char caracter);
+int unirNumeros(int numero, int resultado);
+int convertirEntrada(char entrada[]);
+void limpiarConsola();
+
+//Fase 1: Menu
+int menu();
+
+//Fase 2: Creación de tableros
+int validarDimensiones(int filas, int columnas);
+bool verificarDimensiones(int filas, int columnas);
+void dimensionador();
+//void mostrarMatriz(char matriz[][], int filas, int columnas);
